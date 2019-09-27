a525fc91339ff10c25650eff9605bfd6f5e09ccc
dev8 <serhii.kozak@magefan.com>
2019-09-27 17:51:35 +0300

    changed files, fixed routes

--- registration.php
+++ registration.php
@@ -1,7 +1,6 @@
 <?php
-
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magefan_BlogCustom',
+    'Magefan_BlogAmp',
     __DIR__
 );
